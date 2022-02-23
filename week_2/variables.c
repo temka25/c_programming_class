41a297594bb8494e70e91ce86e2e568168223a90
temka25 <99777154+temka25@users.noreply.github.com>
2022-02-23 10:56:58 +0800

    Create variables.c

---
+++ week_2/variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int myPhoneNumber =80199954;
+    //printf("%d",a);
+    printf("my phone number is %d.\n", myPhoneNumber);
+
+    double  myHeight = 1.8;
+    printf("my Height is %lf.\n" , myHeight);
+
+    char myletter ='T';
+    printf("My Letter is %c.\n", myletter);
+
+    char myName[] = "Temuulen";
+    printf("my name is %s.\n", myName);
+    
+    return 0;
+}
