02200e817a98356bd2695632428d3038182a24a9
temka25 <99777154+temka25@users.noreply.github.com>
2022-02-23 10:59:30 +0800

    Create shuleg.c

---
+++ week_2/shuleg.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+printf("Цаг бол эрдэнэ шүү\n");
+printf("Цаашаа явбал эргэхгүй шүү\n");
+printf("Юу хийх цагтаа\n");
+printf("Түүнийгээ бид заавал хийнэ\n");
+printf("Цаг бол эрдэнэ шүү\n");
+printf("Цаашаа явбал эргэхгүй шүү.\n");
+   return 0;
+}
